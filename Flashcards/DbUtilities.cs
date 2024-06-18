9c2dfe7f9fa5ac2c3f714a38f1f1b536a5882977
aishyuu <ivanmatias1331@gmail.com>
2024-06-17 21:43:53 -0700

    Add project files.

---
+++ Flashcards/DbUtilities.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace Flashcards
+{
+    internal class DbUtilities
+    {
+        public static void Initialize()
+        {
+            var connectionString = "Server=localhost\\SQLEXPRESS;Initial Catalog=DapperDB; Integrated Security = true;TrustServerCertificate=True";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var sql = @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Stacks')
+                            CREATE TABLE Stacks (
+                            Id int IDENTITY(1,1) NOT NULL,
+                            Name NVARCHAR(30) NOT NULL UNIQUE,
+                            PRIMARY KEY (Id)
+                            );";
+
+                connection.Execute(sql);
+            }
+        }
+    }
+}
