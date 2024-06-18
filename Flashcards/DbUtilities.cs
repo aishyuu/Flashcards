9e55667f11f6a59d29f0b209eb45d0b659f43b67
aishyuu <ivanmatias1331@gmail.com>
2024-06-17 22:01:07 -0700

    Changed name of DB and added query for creating flashcards table.

--- Flashcards/DbUtilities.cs
+++ Flashcards/DbUtilities.cs
@@ -7,18 +7,39 @@ namespace Flashcards
     {
         public static void Initialize()
         {
-            var connectionString = "Server=localhost\\SQLEXPRESS;Initial Catalog=DapperDB; Integrated Security = true;TrustServerCertificate=True";
+            var connectionString = "Server=localhost\\SQLEXPRESS;Initial Catalog=FlashcardsDB; Integrated Security = true;TrustServerCertificate=True";
 
-            using (var connection = new SqlConnection(connectionString))
+            try
             {
-                var sql = @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Stacks')
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    var sql = @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Stacks')
                             CREATE TABLE Stacks (
                             Id int IDENTITY(1,1) NOT NULL,
                             Name NVARCHAR(30) NOT NULL UNIQUE,
                             PRIMARY KEY (Id)
                             );";
 
-                connection.Execute(sql);
+                    connection.Execute(sql);
+
+                    sql = @"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'Flashcards')
+                        CREATE TABLE Flashcards(
+                        Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+                        Question NVARCHAR(30) NOT NULL,
+                        Answer NVARCHAR(30) NOT NULL,
+                        StackId int NOT NULL
+                            FOREIGN KEY
+                            REFERENCES Stacks(Id)
+                            ON DELETE CASCADE
+                            ON UPDATE CASCADE
+                        );";
+
+                    connection.Execute(sql);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
