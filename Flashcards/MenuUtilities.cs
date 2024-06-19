326859c5a592fc7a20c0ff1f5779c5d72fed4234
aishyuu <ivanmatias1331@gmail.com>
2024-06-18 18:02:11 -0700

    Added main menu and respective prompts.

---
+++ Flashcards/MenuUtilities.cs
@@ -0,0 +1,35 @@
+﻿using Spectre.Console;
+
+namespace Flashcards
+{
+    internal class MenuUtilities
+    {
+        internal static string MainMenu()
+        {
+            string decision = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Select your [green]action[/]!")
+                .AddChoices(new[]
+                {
+                    "Create New Stack", "Create New Flashcard", "Start Study Session"
+                }));
+
+            return decision;
+        }
+
+        internal static void NewStackPrompts()
+        {
+            AnsiConsole.MarkupLineInterpolated($"[red]New Stack Chosen[/]");
+        }
+
+        internal static void NewFlashcardPrompts()
+        {
+            AnsiConsole.MarkupLineInterpolated($"[yellow]New Flashcard Chosen[/]");
+        }
+
+        internal static void StudySession()
+        {
+            AnsiConsole.MarkupLineInterpolated($"[blue]Study Session Chosen[/]");
+        }
+    }
+}
--- Flashcards/Program.cs
+++ Flashcards/Program.cs
@@ -1,3 +1,21 @@
 ﻿using Flashcards;
+using Spectre.Console;
 
 DbUtilities.Initialize();
+
+var decision = MenuUtilities.MainMenu();
+
+switch(decision)
+{
+    case "Create New Stack":
+        MenuUtilities.NewStackPrompts();
+        break;
+
+    case "Create New Flashcard":
+        MenuUtilities.NewFlashcardPrompts();
+        break;
+
+    case "Start Study Session":
+        MenuUtilities.StudySession();
+        break;
+}
